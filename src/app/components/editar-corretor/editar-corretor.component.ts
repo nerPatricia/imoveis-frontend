b424303a60ba6dded26725d739676900e589ba61
Alison Maciel <alisonmaciel93@gmail.com>
2021-07-15 19:35:00 -0300

    inserção dos selects no component #8

--- src/app/components/editar-corretor/editar-corretor.component.ts
+++ src/app/components/editar-corretor/editar-corretor.component.ts
@@ -9,6 +9,8 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class EditarCorretorDialogComponent implements OnInit {
   form: FormGroup;
+  formSelect: FormGroup;
+  tipoCorretor = ['Contratado', 'Comissionado'];
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
@@ -16,6 +18,7 @@ export class EditarCorretorDialogComponent implements OnInit {
     private fb: FormBuilder,
   ) { 
     console.log(data);
+    
     this.form = this.fb.group({
       creci: new FormControl(data.corretor?.creci || '', [Validators.required]),
       nome: new FormControl(data.corretor?.nome || '', [Validators.required]),
@@ -23,7 +26,23 @@ export class EditarCorretorDialogComponent implements OnInit {
       dataAdmissao: new FormControl(data.corretor?.dataAdmissao || '', [Validators.required]),
       percentComissao: new FormControl(data.corretor?.percentComissao || '', [Validators.required])
     });
-    console.log(this.form);
+    //console.log(this.form);
+
+    this.formSelect = new FormGroup({
+      tipo: new FormControl(this.tipoCorretor[0]),
+    });
+    //console.log(this.formSelect);
+  }
+  
+  getTypesSearch(event) {
+    // TODO: quando seleciona o tipo, exibir apenas imoveis do tipo selecionado
+    console.log("selecionou um tipo de imovel: ", event.value);
+    if(event.value === 'contratado') {
+      console.log('1');
+    }
+    else {
+      console.log('1111');
+    }
   }
 
   ngOnInit(): void {
