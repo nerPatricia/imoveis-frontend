78f08bb9db1efaa258051ba2591cdaa370accfb3
Geovany Mendes <geovany.mendes@mjv.com.br>
2021-07-25 14:16:36 -0300

    [ADD] Upload de imagem

---
+++ src/app/service/file-endpoint.service.ts
@@ -0,0 +1,18 @@
+import { environment } from "../../environments/environment";
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+@Injectable({
+  providedIn: "root",
+})
+export class FileEndpointService {
+  backendUrl = environment.backend;
+
+  constructor(private client: HttpClient) {}
+
+  saveImage(multiPartForm) {
+    const url = this.backendUrl + "/imagem/salvar";
+    return this.client.post(url, multiPartForm).toPromise();
+  }
+}
