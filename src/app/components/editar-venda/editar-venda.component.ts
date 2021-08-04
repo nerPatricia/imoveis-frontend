df567226ff348aa84b16951dd466582c947f6eaa
Shinku159 <shinku.itajuba@outlok.com>
2021-08-04 03:40:12 -0300

    editarVenda

--- src/app/components/editar-venda/editar-venda.component.ts
+++ src/app/components/editar-venda/editar-venda.component.ts
@@ -1,3 +1,4 @@
+import Swal from 'sweetalert2';
 import { Component, OnInit, Inject } from '@angular/core';
 import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 import { VendasEndpointService } from 'src/app/service/vendas-endpoint.service';
@@ -10,7 +11,6 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class EditarVendaDialogComponent implements OnInit {
   titulo = "";
-  edit = false;
   form: FormGroup;
 
   constructor(
@@ -28,8 +28,10 @@ export class EditarVendaDialogComponent implements OnInit {
       creciCorretor: new FormControl(data.venda?.creciCorretor || '', [Validators.required]),
       dataVenda: new FormControl(data.venda?.dataVenda || '', [Validators.required])
     });
+    if (this.data.venda) {
+      this.form.get('codigoImovel').disable();
+    }
     this.titulo = data.venda  ? "EDITAR VENDA" : "CADASTRAR VENDAS";
-    this.edit = data.venda  ? true : false;
   }
 
   getErrorMessage(field) {
@@ -48,19 +50,45 @@ export class EditarVendaDialogComponent implements OnInit {
   }
 
   atualizar() {
-    console.log(this.form.value);
+    this.formataForm();
 
-    this.vendasService.addVenda(this.form.value).then(
-      (response) => {
-        console.log(response);
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
+    if(this.data.venda){
+      this.vendasService
+        .updateVendaById(this.form.value, this.data.venda._id)
+        .then(
+          (response) => {
+            Swal.fire('Imovel atualizado com sucesso', '', 'success').then(() =>
+              window.location.reload()
+            );
+          },
+          (error) => {
+            console.log(error);
+          }
+        );
+    } else {  
+      this.vendasService.addVenda(this.form.value).then(
+        (response) => {
+          console.log(response);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    }
   }
 
-  cadastrar() {
-    console.log(this.form.value);
+  formataForm() {
+
+    if (this.form.get('dataVenda').value.includes('/')) {
+      // se a data incluir uma / quer dizer q nao ta no formato date, entao tem q formatar
+      const dia = this.form.get('dataVenda').value.split('/')[0];
+      const mes = this.form.get('dataVenda').value.split('/')[1];
+      const ano = this.form.get('dataVenda').value.split('/')[2];
+      this.form
+        .get('dataDeCadastro')
+        .setValue(
+          ano + '-' + ('0' + mes).slice(-2) + '-' + ('0' + dia).slice(-2)
+        );
+    }
   }
 }
